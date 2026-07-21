a8302e5ed5e0406454e05efe3b4071b1cbe7558d
Saavari <saavariagr05@gmail.com>
2026-07-21 19:17:45 +0530

    feat: add Analysis page with speech feedback display

--- frontend/src/pages/Analysis.js
+++ frontend/src/pages/Analysis.js
@@ -1,5 +1,37 @@
+import {useState, useEffect} from 'react';
+import {useParams, useNavigate} from 'react-router-dom';
+import API from '../api/axios';
+
 function Analysis(){
-    return <h1>Analysis Page</h1>
+
+
+    const [analysis, setAnalysis] = useState(null);
+    const {speechId} = useParams();
+
+    useEffect(()=>{
+        API.get(`/analysis/${speechId}`)
+        .then(analysis =>{
+            setAnalysis(analysis.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }, []);
+
+    if (!analysis) return <p>Loading...</p>;
+
+    return (
+        <div>
+            <h1>Analysis</h1>
+
+            <p>Transcript: {analysis.transcript_highlighted}</p>
+            <p>Filler word count: {analysis.filler_words_count}</p>
+            <p>Pace: {analysis.pace_wpm}</p>
+            <p>Relevance score: {analysis.relevance_score}</p>
+            <p>Tone consistency score: {analysis.tone_consistency_score}</p>
+            <p>Overall feedback: {analysis.overall_feedback}</p>
+        </div>
+    )
 }
 
 export default Analysis;
